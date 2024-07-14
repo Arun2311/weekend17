627545c345f5c54743ebbc37602978b2accde200
arunachalam2311 <arunachalam.v@doodleblue.in>
2024-07-14 14:40:45 +0530

    j

--- src/Component/FunComp/ApiCall.jsx
+++ src/Component/FunComp/ApiCall.jsx
@@ -1,39 +1,41 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function ApiCall() {
-    const [count, setcount] = useState(1);
-    const [extracount, setextracount] = useState(2);
+  const [count, setcount] = useState(1);
+  const [extracount, setextracount] = useState(2);
+
+  const handleadd = () => {
+    setcount(count + 1);
+  };
 
 
+  
 
-    const handleadd = () => {
-      setcount(count + 1);
+  useEffect(() => {
+    console.log("Arun");
+
+    return () => {
+      console.log("logut");
     };
+  }, [extracount, count]);
 
 
 
-    useEffect(()=>{
-        console.log("Arun");
-    }, [extracount,count])
-
-
-    const handleextraadd =()=>{
-        setextracount(extracount+1)
-    }
-
+  const handleextraadd = () => {
+    setextracount(extracount + 1);
+  };
 
   return (
     <div>
-        <h1>Life Cycleusing hook its useeffect </h1>
+      <h1>Life Cycleusing hook its useeffect </h1>
 
-        <h2>count : {count}</h2>
+      <h2>count : {count}</h2>
 
-        <h2>extracount : {extracount}</h2>
+      <h2>extracount : {extracount}</h2>
 
-        <button onClick={handleadd}>Add</button>
+      <button onClick={handleadd}>Add</button>
 
-        <button onClick={handleextraadd}>extra Add</button>
-
+      <button onClick={handleextraadd}>extra Add</button>
     </div>
-  )
+  );
 }
